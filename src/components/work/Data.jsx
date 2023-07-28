8aa080fced5e4335f97d3f636573d7c7e241a809
saurabhkp_2001 <saurabhkp2001@gmail.com>
2023-07-28 21:12:20 +0530

    initial commit

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -2,37 +2,39 @@ import Works1 from "../../assets/projects/projectOne.jpg";
 import Works2 from "../../assets/projects/projectTwo.jpg";
 import Works3 from "../../assets/projects/projectThree.jpg";
 import Works4 from "../../assets/projects/projectFour.jpg";
-import Works5 from "../../assets/projects/projectOne.jpg";
+import Works5 from "../../assets/projects/mistore.jpg";
 
 export const projectData=[
     {
         id:1,
+        image: Works5,
+        title:'MI store clone',
+        url:"https://sunny-caramel-ffd93d.netlify.app"
+    },
+    {
+        id:2,
         image: Works1,
         title:'Ecommerce Website',
         url:"https://saurabhkp2001.github.io/E-Commerce/index.html"
     },
     {
-        id:2,
+        id:3,
         image: Works4,
         title:'Keeper App',
         url:"https://taupe-sfogliatella-7231d5.netlify.app/",
     },
     {
-        id:3,
+        id:4,
         image: Works2,
         title:'Weather App',
         url:"https://saurabhkp2001.github.io/Weather-App/"
     },
     {
-        id:4,
+        id:5,
         image: Works3,
         title:'Zomato Clone Frontend',
         url:"https://saurabhkp2001.github.io/Zomato-clon/"
     },
     
-    {
-        id:5,
-        image: Works5,
-        title:'Web Design',
-    },
+   
 ]
