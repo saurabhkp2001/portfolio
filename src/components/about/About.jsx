7c126a903f3f9b5acc5c4468c14117dacf55d7bf
saurabhkp_2001 <saurabhkp2001@gmail.com>
2024-05-28 01:23:52 +0530

    initial commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./about.css";
 import AboutImg from "../../assets/image2.jpg";
-import CV from "../../assets/SaurabhCV.pdf";
+import CV from "../../assets/SaurabhResume1.pdf";
 import Info from "./Info";
 
 const About = () => {
--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -4,40 +4,47 @@ import Works3 from "../../assets/projects/projectThree.jpg";
 import Works4 from "../../assets/projects/projectFour.jpg";
 import Works5 from "../../assets/projects/mistore.jpg";
 import Works6 from "../../assets/projects/dotolist.jpg";
+import Works7 from "../../assets/projects/socialMedia.png";
 
 export const projectData=[
     {
         id:1,
+        image: Works7,
+        title:'Social Media Application using MERN',
+        url:"https://taupe-horse-fc617f.netlify.app"
+    },
+    {
+        id:2,
         image: Works5,
         title:'MI store clone',
         url:"https://sunny-caramel-ffd93d.netlify.app"
     },
     {
-        id:2,
+        id:3,
         image: Works1,
         title:'Ecommerce Website',
         url:"https://saurabhkp2001.github.io/E-Commerce/index.html"
     },
     {
-        id:3,
+        id:4,
         image: Works4,
         title:'Keeper App',
         url:"https://taupe-sfogliatella-7231d5.netlify.app/",
     },
     {
-        id:4,
+        id:5,
         image: Works2,
         title:'Weather App',
         url:"https://saurabhkp2001.github.io/WeatherApp/"
     },
     {
-        id:5,
+        id:6,
         image: Works6,
         title:'To-do-list',
         url:"https://saurabhkp2001.github.io/to-do-list/"
     },
     {
-        id:6,
+        id:7,
         image: Works3,
         title:'Fully Responsive E-Commerce Website',
         url:"https://astonishing-biscotti-e9587c.netlify.app/"
